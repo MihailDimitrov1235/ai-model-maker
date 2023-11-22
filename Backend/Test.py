ecaa38de6820b4ad771e185d083b65a3dd23861d
MihailDimitrov1235 <mighty.strong1235@gmail.com>
2023-11-22 12:25:45 +0200

    trying to run python files

--- Backend/Test.py
+++ Backend/Test.py
@@ -2,63 +2,64 @@
 # import pandas as pd
 # import seaborn as sns
 
-import tensorflow as tf
+# import tensorflow as tf
 
-import os
+# import os
 
+print('test')
 
 # print("GPUs Available: ", tf.config.list_physical_devices('GPU'))
 
 
-def is_file_in_folder(folder_path, file_name):
-    file_path = os.path.join(folder_path, file_name)
-    return os.path.isfile(file_path)
+# def is_file_in_folder(folder_path, file_name):
+#     file_path = os.path.join(folder_path, file_name)
+#     return os.path.isfile(file_path)
 
-folder = 'Backend/models'
-file = 'test.keras'
-path = folder+'/'+file
-# Loading data
+# folder = 'Backend/models'
+# file = 'test.keras'
+# path = folder+'/'+file
+# # Loading data
 
-mnist = tf.keras.datasets.mnist
+# mnist = tf.keras.datasets.mnist
 
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-x_train, x_test = x_train / 255.0, x_test / 255.0
+# (x_train, y_train), (x_test, y_test) = mnist.load_data()
+# x_train, x_test = x_train / 255.0, x_test / 255.0
 
-if(is_file_in_folder(folder, file)):
-    print('loading model')
-    model = tf.keras.models.load_model(path)
-else:
-    print('training model')
-    # Creating model
-    model = tf.keras.models.Sequential([
-      tf.keras.layers.Flatten(input_shape=(28, 28)),
-      tf.keras.layers.Dense(128, activation='relu'),
-      tf.keras.layers.Dropout(0.2),
-      tf.keras.layers.Dense(10)
-    ])
+# if(is_file_in_folder(folder, file)):
+#     print('loading model')
+#     model = tf.keras.models.load_model(path)
+# else:
+#     print('training model')
+#     # Creating model
+#     model = tf.keras.models.Sequential([
+#       tf.keras.layers.Flatten(input_shape=(28, 28)),
+#       tf.keras.layers.Dense(128, activation='relu'),
+#       tf.keras.layers.Dropout(0.2),
+#       tf.keras.layers.Dense(10)
+#     ])
 
-    predictions = model(x_train[:1]).numpy()
+#     predictions = model(x_train[:1]).numpy()
 
-    tf.nn.softmax(predictions).numpy()
+#     tf.nn.softmax(predictions).numpy()
 
-    loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+#     loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
 
-    loss_fn(y_train[:1], predictions).numpy()
+#     loss_fn(y_train[:1], predictions).numpy()
 
-    model.compile(optimizer='adam',
-                  loss=loss_fn,
-                  metrics=['accuracy'])
+#     model.compile(optimizer='adam',
+#                   loss=loss_fn,
+#                   metrics=['accuracy'])
 
-    # Training
+#     # Training
 
-    model.fit(x_train, y_train, epochs=1)
+#     model.fit(x_train, y_train, epochs=1)
 
-    model.save(path)
+#     model.save(path)
 
-model.evaluate(x_test,  y_test, verbose=2)
+# model.evaluate(x_test,  y_test, verbose=2)
 
-probability_model = tf.keras.Sequential([
-  model,
-  tf.keras.layers.Softmax()
-])
+# probability_model = tf.keras.Sequential([
+#   model,
+#   tf.keras.layers.Softmax()
+# ])
 # probability_model(x_test[:5])
--- Frontend/src/Pages/Landing.js
+++ Frontend/src/Pages/Landing.js
@@ -1,9 +1,24 @@
 import { Button } from "@mui/material"
+// import python from 'python-shell';
+// import path from "path";
+
+// var options = {
+//     scriptPath : path.join(__dirname, '/../Backend/'),
+//     args : [],
+// };
+
+
 
 const Landing = function(){
+    const click = () => {
+        // let test = new python('Test.py', options);
+        // test.on('message', function(message){
+        //     console.log(message)
+        // })
+    }
     return(
         <>
-            <Button style={{maxWidth:'100px'}}>sus</Button>
+            <Button onClick={click} style={{maxWidth:'100px'}}>sus</Button>
         </>
     )
 }
