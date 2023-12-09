6a74e4d5bdb9b3f44f7cb4d8101be0457fb6cc9e
alexhr05 <56800932+alexhr05@users.noreply.github.com>
2023-12-09 13:02:47 +0200

    Added card and pagination

--- App/src/Pages/Landing.js
+++ App/src/Pages/Landing.js
@@ -1,6 +1,8 @@
-import { Button } from "@mui/material"
+import { Button, Grid, Box, Pagination  } from "@mui/material"
 import { useTranslation } from 'react-i18next';
 import {ShowDiagrams} from '../Components/Charts/BarChart_GoogleLib'
+import CardElement from "../Components/CardElement";
+
 
 // import python from 'python-shell';
 // import path from "path";
@@ -26,8 +28,31 @@ const Landing = function(){
     }
     return(
         <>
-            <ShowDiagrams/>
-            <Button variant="main" onClick={click} style={{maxWidth:'100px'}}>{t('Test')}</Button>
+            <Box>
+                <Grid container spacing={2}>
+                    <Grid item xs={4}>
+                        <CardElement/>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <CardElement/>
+                    </Grid>                    
+                    <Grid item xs={4}>
+                        <h1>RABOTI</h1>
+                    </Grid>
+                </Grid>
+                <Box sx={{ 
+                    flexGrow: 1,
+                    display: "flex",
+                    justifyContent: "center"
+                }} >
+                    <Pagination count={10} color="primary" />
+                </Box>
+                
+            </Box>
+            
+            
+            {/* <ShowDiagrams/>
+            <Button variant="main" onClick={click} style={{maxWidth:'100px'}}>{t('Test')}</Button> */}
         </>
     )
 }
