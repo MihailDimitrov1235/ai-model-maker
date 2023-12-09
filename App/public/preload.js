ec3a8a09109b78a85150bc0d1f951d29359cbbb5
MihailDimitrov1235 <mighty.strong1235@gmail.com>
2023-12-09 12:37:39 +0200

    fixed an error

--- App/public/preload.js
+++ App/public/preload.js
@@ -4,5 +4,6 @@ contextBridge.exposeInMainWorld('electronAPI', {
   checkVenv: (arg) => ipcRenderer.send('check-venv', arg),
   createVenv: (arg) =>ipcRenderer.send('create-venv', arg),
   runPython: (arg) => ipcRenderer.send('run-python', arg),
-  handleMissingVenv: (callback) => ipcRenderer.on('no-vnev', callback)
+  handleMissingVenv: (callback) => ipcRenderer.on('no-vnev', callback),
+  handleMissingConda: (callback) => ipcRenderer.on('no-conda', callback)
 })
