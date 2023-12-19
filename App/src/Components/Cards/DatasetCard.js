da9ff2c9c94a8314a049293e1a014d520a48bfc6
alexhr05 <56800932+alexhr05@users.noreply.github.com>
2023-12-19 11:02:54 +0200

    Added Icons to Datasets Page

--- App/src/Components/Cards/DatasetCard.js
+++ App/src/Components/Cards/DatasetCard.js
@@ -8,20 +8,38 @@ import {
   CardActions,
   Box,
 } from "@mui/material";
-
+import InsertDriveFileIcon from "@mui/icons-material/InsertDriveFile";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
 export default function DatasetCard({ title }) {
   return (
     <Card sx={{ width: "100%" }}>
       <CardContent>
         <Typography
-          sx={{ fontSize: 18, fontWeight: "bold", mb: 3 }}
+          sx={{
+            fontSize: 18,
+            fontWeight: "bold",
+            mb: 3,
+            display: "flex",
+            justifyContent: "space-between",
+          }}
           color="text.secondary"
           gutterBottom
         >
           {title}
+          <MoreVertIcon />
         </Typography>
 
-        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+        <Typography
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            fontSize: 14,
+            gap: 1,
+          }}
+          color="text.secondary"
+          gutterBottom
+        >
+          <InsertDriveFileIcon />
           Name Datasets
         </Typography>
       </CardContent>
