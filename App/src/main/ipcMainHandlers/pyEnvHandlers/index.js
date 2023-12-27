d4bf89c4bbe886e6b57449d6bd6a4c49bff3e3a5
MihailDimitrov1235 <mighty.strong1235@gmail.com>
2023-12-28 00:14:00 +0200

    refactor: :recycle: cleaned create env code

--- App/src/main/ipcMainHandlers/pyEnvHandlers/index.js
+++ App/src/main/ipcMainHandlers/pyEnvHandlers/index.js
@@ -5,7 +5,6 @@ const { exec } = require('child_process');
 const path = require('path');
 
 let createEnvProcess = null;
-let installLibrariesProcess = null;
 
 const envPath = path.join(__dirname, '../../../../assets/python-scripts/env');
 const appDataPath =
@@ -36,20 +35,8 @@ function setupIPCMainPyEnv(win) {
     }
     const anacondaShortcutPath = condaExists(appDataPath);
     if (anacondaShortcutPath) {
-      const command = `start /B cmd /C "${anacondaShortcutPath}" && conda create --prefix "${envPath}" python=3.10 --yes`;
-      createEnvProcess = exec(`${command}`, (error, stdout, stderr) => {
-        console.log(stdout);
-        if (error) {
-          console.error('Error:', error.message);
-        } else {
-          console.log('Environment setup complete.');
-          ipcMain.emit('install-libraries');
-          win.webContents.send(
-            'change-create-env-text',
-            'installing libraries',
-          );
-        }
-      });
+      const command = `start /B cmd /C "${anacondaShortcutPath}" && conda create --prefix "${envPath}" python=3.10 --yes && set CONDA_BAT=1 && echo yes | conda install -c conda-forge cudatoolkit=11.2 cudnn=8.1.0`;
+      createEnvProcess = exec(`${command}`, (error, stdout, stderr) => {});
 
       createEnvProcess.stdout.on('data', (data) => {
         // Handle the stdout data
@@ -72,56 +59,19 @@ function setupIPCMainPyEnv(win) {
         });
       });
       createEnvProcess.stderr.on('data', (data) => {
-        // Handle the stderr data
         console.log('stderr');
         console.error(data.toString());
+        win.webContents.send('close-create-env');
+        createEnvProcess = null;
       });
       createEnvProcess.on('close', (code) => {
         console.log(`child process exited with code ${code}`);
-        if (!installLibrariesProcess) {
-          win.webContents.send('close-create-env');
-        }
-        createEnvProcess = false;
+        win.webContents.send('close-create-env');
+        createEnvProcess = null;
       });
     } else {
       win.webContents.send('close-create-env');
       win.webContents.send('no-conda');
-    }
-  });
-
-  ipcMain.on('install-libraries', (event, arg) => {
-    console.log('installing libs');
-    const currentEnvPath = envExists(envPath);
-    const anacondaShortcutPath = condaExists(appDataPath);
-
-    if (anacondaShortcutPath && currentEnvPath) {
-      const command = `start /B cmd /C "${anacondaShortcutPath}" && set CONDA_BAT=1 && conda activate "${envPath}" && echo yes | conda install -c  conda-forge cudatoolkit=11.2 cudnn=8.1.0`;
-      installLibrariesProcess = exec(`${command}`, (error, stdout, stderr) => {
-        console.log(stdout);
-        if (error) {
-          console.error('Error:', error.message);
-          win.webContents.send('close-create-env');
-          installLibrariesProcess = null;
-        } else {
-          win.webContents.send('close-create-env');
-          installLibrariesProcess = null;
-        }
-      });
-      installLibrariesProcess.stdout.on('data', (data) => {
-        // Handle the stdout data
-        const lines = data.toString().split('\n');
-        lines.forEach((line) => {
-          console.log(line);
-          if (line.includes('...working...')) {
-            win.webContents.send(
-              'change-create-env-text',
-              line.split(': ...working...')[0],
-            );
-          }
-        });
-      });
-    } else {
-      win.webContents.send('close-create-env');
     }
   });
 
