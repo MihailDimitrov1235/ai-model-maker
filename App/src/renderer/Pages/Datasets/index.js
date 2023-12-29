dfaf05243159db20cffa91f8ea491fdff3ebb153
MihailDimitrov1235 <mighty.strong1235@gmail.com>
2023-12-29 16:56:03 +0200

    style: :art: imporved datasets page code

--- App/src/renderer/Pages/Datasets/index.js
+++ App/src/renderer/Pages/Datasets/index.js
@@ -8,13 +8,12 @@ import {
   InputAdornment,
   Menu,
   MenuItem,
-} from "@mui/material";
-import React from "react";
-import SearchIcon from "@mui/icons-material/Search";
-import { useTranslation } from "react-i18next";
-import { ShowDiagrams } from "../../Components/Charts/BarChart_GoogleLib";
-import CardElement from "../../Components/Cards/DatasetCard";
-import { Link } from "react-router-dom";
+} from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import { useTranslation } from 'react-i18next';
+import { ShowDiagrams } from '../../Components/Charts/BarChart_GoogleLib';
+import CardElement from '../../Components/Cards/DatasetCard';
+import { useNavigate } from 'react-router-dom';
 // import python from 'python-shell';
 // import path from "path";
 
@@ -25,60 +24,43 @@ import { Link } from "react-router-dom";
 
 const Datasets = function () {
   const { t, i18n } = useTranslation();
-
-  const click = () => {
-    //   window.electronAPI.runPython('test')
-    window.electronAPI.checkVnev();
-    // let test = new python('Test.py', options);
-    // test.on('message', function(message){
-    //     console.log(message)
-    // })
-  };
+  const navigate = useNavigate();
 
   const datasets = [
-    { title: "Road Sign", Miho: "waka waka eee", Alvin: "Chiponoskovci" },
-    { title: "Road Sign" },
-    { title: "Road Sign" },
-    { title: "Football Objects" },
-    { title: "Basketball Objects" },
-    { title: "Titanic Deaths" },
+    { title: 'Road Sign', Miho: 'waka waka eee', Alvin: 'Chiponoskovci' },
+    { title: 'Road Sign' },
+    { title: 'Road Sign' },
+    { title: 'Football Objects' },
+    { title: 'Basketball Objects' },
+    { title: 'Titanic Deaths' },
   ];
-
-  const [anchorEl, setAnchorEl] = React.useState(null);
-
-  const open = Boolean(anchorEl);
   const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
+    navigate('/data/import');
   };
 
   return (
     <>
       <Box
         sx={{
-          margin: "50px",
+          margin: '50px',
         }}
       >
         <Box
           sx={{
-            display: "flex",
-            justifyContent: "space-between",
+            display: 'flex',
+            justifyContent: 'space-between',
           }}
         >
-          <h2>DATASETS</h2>
+          <h2>{t('datasets')}</h2>
 
           <Box
             sx={{
-              display: "flex",
-              alignItems: "center",
+              display: 'flex',
+              alignItems: 'center',
               gap: 3,
             }}
           >
             <TextField
-              id="outlined-basic"
               variant="outlined"
               placeholder="Search"
               InputProps={{
@@ -86,62 +68,19 @@ const Datasets = function () {
               }}
               sx={{
                 input: {
-                  padding: "8px",
+                  padding: '8px',
                 },
               }}
             />
-            <Button
-              id="basic-button"
-              aria-controls={open ? "basic-menu" : undefined}
-              aria-haspopup="true"
-              aria-expanded={open ? "true" : undefined}
-              variant="contrast"
-              onClick={handleClick}
-            >
-              New Datasets
+            <Button variant="contrast" onClick={handleClick}>
+              {t('new-dataset')}
             </Button>
-            <Menu
-              id="basic-menu"
-              anchorEl={anchorEl}
-              open={open}
-              onClose={handleClose}
-              MenuListProps={{
-                "aria-labelledby": "basic-button",
-              }}
-            >
-              <Link to={"table/import"}>
-                <MenuItem
-                  onClick={handleClose}
-                  sx={{
-                    color: "#000",
-                  }}
-                >
-                  Tabular Data
-                </MenuItem>
-                <MenuItem
-                  onClick={handleClose}
-                  sx={{
-                    color: "#000",
-                  }}
-                >
-                  Image Data
-                </MenuItem>
-                <MenuItem
-                  onClick={handleClose}
-                  sx={{
-                    color: "#000",
-                  }}
-                >
-                  Text Data
-                </MenuItem>
-              </Link>
-            </Menu>
           </Box>
         </Box>
 
         <Box
           sx={{
-            margin: "25px",
+            margin: '25px',
           }}
         >
           <Grid container spacing={4}>
@@ -154,9 +93,9 @@ const Datasets = function () {
           <Box
             sx={{
               flexGrow: 1,
-              display: "flex",
-              justifyContent: "center",
-              margin: "30px",
+              display: 'flex',
+              justifyContent: 'center',
+              margin: '30px',
             }}
           >
             <Pagination count={10} color="primary" />
