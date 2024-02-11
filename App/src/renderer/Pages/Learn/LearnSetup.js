d57ea5ad9f5966b0cb37f72fd0f247245570368b
MihailDimitrov1235 <mighty.strong1235@gmail.com>
2024-02-11 20:49:00 +0200

    docs: :memo: added CPU installation guide

--- App/src/renderer/Pages/Learn/LearnSetup.js
+++ App/src/renderer/Pages/Learn/LearnSetup.js
@@ -12,9 +12,11 @@ import {
 } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import CodeBlock from '../../Components/CodeBlock';
+import { useNavigate } from 'react-router-dom';
 
 export default function LearnSetup() {
   const { t } = useTranslation();
+  const navigate = useNavigate();
   const theme = useTheme();
   const [value, setValue] = useState(0);
 
@@ -22,10 +24,8 @@ export default function LearnSetup() {
     setValue(newValue);
   };
 
-  const handleOpenUrl = () => {
-    window.electronAPI.openUrl(
-      'https://learn.microsoft.com/en-US/cpp/windows/latest-supported-vc-redist?view=msvc-170',
-    );
+  const handleOpenUrl = (url) => {
+    window.electronAPI.openUrl(url);
   };
 
   return (
@@ -55,8 +55,97 @@ export default function LearnSetup() {
         <Tab label={'CPU'} value={0} />
         <Tab label={'GPU'} value={1} />
       </Tabs>
-      <Box display={value == 0 ? 'block' : 'none'}>
-        <Typography variant="h5">1. System requirements</Typography>
+      <Box
+        display={value == 0 ? 'flex' : 'none'}
+        sx={{ flexDirection: 'column', gap: 3 }}
+      >
+        <Box>
+          <Typography variant="h5">
+            {`1. Install Python (Python 3.8 – 3.11)`}
+          </Typography>
+          <Typography variant="p">
+            If Python is not already installed on your system, download and
+            install a version between Python 3.8 and Python 3.11 from the{' '}
+            <Link
+              sx={{ cursor: 'pointer' }}
+              onClick={() => handleOpenUrl('https://www.python.org/downloads/')}
+            >
+              official Python website
+            </Link>
+            . Make sure to check the option to add Python to your system PATH
+            during installation.
+          </Typography>
+        </Box>
+
+        <Box>
+          <Typography variant="h5">2. Open Command Prompt</Typography>
+          <Typography variant="p">
+            Press <span style={{ fontWeight: 'bold' }}>Win + R</span>, type{' '}
+            <span style={{ fontWeight: 'bold' }}>cmd</span> and press Enter to
+            open the Command Prompt.
+          </Typography>
+        </Box>
+
+        <Box>
+          <Typography variant="h5">3. Create a Virtual Environment</Typography>
+          <CodeBlock language={'bash'} code={`python -m venv tf`} />
+        </Box>
+
+        <Box>
+          <Typography variant="h5">
+            4. Activate the Virtual Environment
+          </Typography>
+          <Typography variant="p">
+            Activate the virtual environment by running the following command
+          </Typography>
+          <CodeBlock language={'bash'} code={`tf\\Scripts\\activate`} />
+        </Box>
+
+        <Box>
+          <Typography variant="h5">5. Install TensorFlow</Typography>
+          <Typography variant="p">
+            Once the virtual environment is activated, install TensorFlow using
+            pip
+          </Typography>
+          <CodeBlock language={'bash'} code={`pip install tensorflow`} />
+        </Box>
+
+        <Box>
+          <Typography variant="h5">5. Install TensorFlow</Typography>
+          <Typography variant="p">
+            Once the virtual environment is activated, install TensorFlow using
+            pip
+          </Typography>
+          <CodeBlock language={'bash'} code={`pip install tensorflow`} />
+        </Box>
+
+        <Typography variant="h5">6. Selecting the python executable</Typography>
+
+        <List
+          sx={{
+            listStyleType: 'disc',
+            pl: 2,
+            '& .MuiListItem-root': {
+              display: 'list-item',
+            },
+          }}
+        >
+          <ListItem>Go to the settings on the bottom left</ListItem>
+          <ListItem>
+            Select the path to the python executable of the environment you just
+            created
+          </ListItem>
+        </List>
+        <Box>
+          <Typography variant="p">
+            If you dont know what the path to the environment is you can run
+          </Typography>
+          <CodeBlock language={'bash'} code={`where python`} />
+        </Box>
+        <Alert variant="filled" severity="info" sx={{ color: 'text.contrast' }}>
+          By default the path should be:
+          C:\Users\name-of-your-user\tf\Scripts\python.exe
+        </Alert>
       </Box>
       <Box
         display={value == 1 ? 'flex' : 'none'}
@@ -91,7 +180,14 @@ export default function LearnSetup() {
           >
             <ListItem>
               {'Go to the '}
-              <Link sx={{ cursor: 'pointer' }} onClick={handleOpenUrl}>
+              <Link
+                sx={{ cursor: 'pointer' }}
+                onClick={() =>
+                  handleOpenUrl(
+                    'https://learn.microsoft.com/en-US/cpp/windows/latest-supported-vc-redist?view=msvc-170',
+                  )
+                }
+              >
                 {'Microsoft Visual C++ downloads'}
               </Link>
               {/* <Button onClick={handleOpenUrl}>button</Button> */}
@@ -137,21 +233,6 @@ export default function LearnSetup() {
               code={`conda create --name tf python=3.9`}
             />
           </Box>
-          {/* <Box>
-            <Typography variant="p">
-              You should recieve a message like this
-            </Typography>
-            <CodeBlock
-              language="bash"
-              code={`# To activate this environment, use
-#
-#     $ conda activate tf
-#
-# To deactivate an active environment, use
-#
-#     $ conda deactivate`}
-            />
-          </Box> */}
 
           <Box>
             <Typography variant="p">Activate the environment</Typography>
@@ -199,10 +280,6 @@ conda activate tf`}
               Select the path to the python executable of the environment you
               just created
             </ListItem>
-            <ListItem>
-              Download and install the Microsoft Visual C++ Redistributable for
-              Visual Studio 2015, 2017 and 2019 for your platform.
-            </ListItem>
           </List>
           <Box>
             <Typography variant="p">
@@ -220,6 +297,27 @@ conda activate tf`}
             C:\Users\name-of-your-user\miniconda3\envs\tf\python.exe
           </Alert>
         </Box>
+      </Box>
+      <Box width={'100%'} mb={3}>
+        <Typography variant="p">
+          If followed the instructions to this point the enviornment should be
+          set up and you should have the ability to train your own models. You
+          can close everything you had to open and can proceed with the{' '}
+          <Link
+            sx={{ cursor: 'pointer' }}
+            onClick={() => navigate('/learn/tutorial')}
+          >
+            Tutorial
+          </Link>{' '}
+          which will guide you on how to create and use your own models . In
+          case you faced any issues you can check the full{' '}
+          <Link
+            sx={{ cursor: 'pointer' }}
+            onClick={() => handleOpenUrl('https://www.tensorflow.org/install')}
+          >
+            Tensorflow installation guide
+          </Link>
+        </Typography>
       </Box>
     </Box>
   );
